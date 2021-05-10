f76f68f564d6c7c217e36556cf763917a29051fc
kunu2804 <83916349+kunu2804@users.noreply.github.com>
2021-05-10 20:12:24 +0530

    Add files via upload

---
+++ SJF without Arrival Time.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+using namespace std;
+int mat[10][6];
+
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void completionTime(int num, int mat[][6])
+{
+	int temp, val;
+	mat[0][3] = mat[0][1] + mat[0][2];
+	mat[0][5] = mat[0][3] - mat[0][1];
+	mat[0][4] = mat[0][5] - mat[0][2];
+	
+	for(int i=1; i<num; i++)
+	{
+		temp = mat[i-1][3];
+		int low = mat[i][2];
+		for(int j=i; j<num; j++)
+		{
+			if(temp >= mat[j][1] && low >= mat[j][2])
+			{
+				low = mat[j][2];
+				val = j;
+			}
+		}
+		mat[val][3] = temp + mat[val][2];
+		mat[val][5] = mat[val][3] - mat[val][1];
+		mat[val][4] = mat[val][5] - mat[val][2];
+		for(int k=0; k<6; k++)
+		{
+			swap(mat[val][k], mat[i][k]);
+		}
+	}
+}
+
+int main()
+{
+	int num, temp;
+	
+	cout<<"Enter number of Process: ";
+	cin>>num;
+	
+	cout<<"...Enter the process ID...\n";
+	for(int i=0; i<num; i++)
+	{
+		cout<<"...Process "<<i+1<<"...\n";
+		cout<<"Enter Process Id: ";
+		cin>>mat[i][0];
+    mat[i][1]=0;
+		cout<<"Enter Burst Time: ";
+		cin>>mat[i][2];
+	}
+	
+	cout<<"Before Arrange...\n";
+	cout<<"Process ID\tArrival Time\tBurst Time\n";
+	for(int i=0; i<num; i++)
+	{
+		cout<<mat[i][0]<<"\t\t"<<mat[i][1]<<"\t\t"<<mat[i][2]<<"\n";
+	}
+	completionTime(num, mat);
+	cout<<"Final Result...\n";
+	cout<<"Process ID\tArrival Time\tBurst Time\tWaiting Time\tTurnaround Time\n";
+	for(int i=0; i<num; i++)
+	{
+		cout<<mat[i][0]<<"\t\t"<<mat[i][1]<<"\t\t"<<mat[i][2]<<"\t\t"<<mat[i][4]<<"\t\t"<<mat[i][5]<<"\n";
+	}
+}
